e10a9f8e9b7c9e16d1190e4dcb8b5031733ea4c8
bensonyj <yingjian@yingjiandeiMac.lan>
2016-06-03 18:30:40 +0800

    swift—控制流(未完成)

--- chapter1_test/MyPlayground_ControlFlow.playground/Contents.swift
+++ chapter1_test/MyPlayground_ControlFlow.playground/Contents.swift
@@ -10,3 +10,83 @@ for (animalName,legCount) in numberOfLegs{
 }
 
 
+let finalSquare = 25
+var board = [Int](count:finalSquare+1,repeatedValue:0)
+board[03] = +08
+board[06] = +11
+board[09] = +09
+board[10] = +02
+board[14] = -10
+board[19] = -11
+board[22] = -02
+
+var square = 0
+var diceRoll = 0
+while square < finalSquare{
+    //掷骰子
+    diceRoll += 1
+    if diceRoll == 7 {
+        diceRoll = 1
+    }
+    
+    //根据点数移动
+    square += diceRoll
+    if square < board.count{
+        //如果玩家还在棋盘上，顺着梯子爬上去或者顺着蛇滑下去
+        square += board[square]
+    }
+    
+    let string = "骰子点数:" + String(diceRoll)
+    
+    print(string)
+}
+
+square = 0
+diceRoll = 0
+
+repeat{
+    //顺着梯子爬上去或者顺着蛇滑下去
+    square += board[square]
+    
+    //掷骰子
+    diceRoll += 1
+    if diceRoll == 7 {
+        diceRoll = 1
+    }
+    //更加点数移动
+    square += diceRoll
+}while square < finalSquare
+
+print("Game over!")
+
+//switch
+let someCharacter: Character = "e"
+switch someCharacter{
+    case "a","e","i","o","u":
+        print("\(someCharacter) is a vowel")
+    case "b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z":
+        print("\(someCharacter) is a consonant")
+    default:
+        print("\(someCharacter) is not a vowel or a consonant")
+}
+
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+var naturalCount: String
+switch approximateCount{
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "serveral"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount)\(naturalCount).")
+
+        
